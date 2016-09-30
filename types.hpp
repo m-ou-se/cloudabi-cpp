36d73c9a3170f78d7ec20d292fe31e23101e3285
Maurice Bos <m-ou.se@m-ou.se>
2016-09-30 15:06:35 +0200

    Update types.hpp.

--- types.hpp
+++ types.hpp
@@ -5,10 +5,17 @@
 namespace cloudabi {
 
 using raw::backlog;
+using raw::clockid;
+using raw::exitcode;
 using raw::filesize;
 using raw::filetype;
+using raw::mflags;
+using raw::mprot;
 using raw::sdflags;
+using raw::signal;
 using raw::ssflags;
+using raw::tid;
+using raw::timestamp;
 using raw::ulflags;
 
 }
