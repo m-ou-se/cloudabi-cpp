23ead1458df8c1833e9ea3cb6de72f5235727d77
Maurice Bos <m-ou.se@m-ou.se>
2016-09-30 12:08:18 +0200

    Update fd and add unique_fd.
    fd is now non-owning and trivially {destructible,copyable}.

--- fd.hpp
+++ fd.hpp
@@ -1,58 +1,61 @@
+#include <memory>
+
 #include <cloudabi_types.h>
 #include <cloudabi_syscalls.h>
 
 #include "error.hpp"
 #include "error_or.hpp"
+#include "string_view.hpp"
+#include "types.hpp"
 
 namespace cloudabi {
 
-class fd {
+struct fd;
+
+struct fd_closer {
+	using pointer = fd;
+	void operator () (fd f);
+};
+
+using unique_fd = std::unique_ptr<fd, fd_closer>;
+
+struct fd {
+
+private:
 	cloudabi_fd_t fd_ = -1;
 
 public:
+
 	fd() noexcept {}
+	fd(std::nullptr_t) noexcept {}
+	explicit fd(cloudabi_fd_t fd) noexcept : fd_(fd) {}
 
-	fd(fd && other) noexcept : fd_(other.release_fd()) {}
+	explicit operator bool() const { return fd_ != cloudabi_fd_t(-1); }
 
-	~fd() { close(); }
-
-	explicit fd(cloudabi_fd_t fd) : fd_(fd) {}
-
-	cloudabi_fd_t release_fd() noexcept {
-		auto fd = fd_;
-		fd_ = -1;
-		return fd;
-	}
-
-	fd & operator = (fd && other) noexcept {
-		close();
-		fd_ = other.release_fd();
-		return *this;
-	}
+	friend bool operator == (fd a, fd b) { return a.fd_ == b.fd_; }
+	friend bool operator != (fd a, fd b) { return a.fd_ != b.fd_; }
 
 	// cloudabi_sys_fd_ syscalls.
 
 	error close() {
-		auto err = cloudabi_sys_fd_close(fd_);
-		fd_ = -1;
-		return error(err);
+		return error(cloudabi_sys_fd_close(fd_));
 	}
 
-	static error_or<fd> create_poll() {
-		cloudabi_fd_t result;
-		if (auto err = cloudabi_sys_fd_create1(CLOUDABI_FILETYPE_POLL, &result)) {
+	static error_or<unique_fd> create1() {
+		fd result;
+		if (auto err = cloudabi_sys_fd_create1(CLOUDABI_FILETYPE_POLL, &result.fd_)) {
 			return error(err);
 		} else {
-			return fd(result);
+			return unique_fd(result);
 		}
 	}
 
-	static error_or<std::pair<fd, fd>> create_pipe() {
-		cloudabi_fd_t a, b;
-		if (auto err = cloudabi_sys_fd_create2(CLOUDABI_FILETYPE_FIFO, &a, &b)) {
+	static error_or<std::pair<unique_fd, unique_fd>> create_pipe() {
+		fd a, b;
+		if (auto err = cloudabi_sys_fd_create2(CLOUDABI_FILETYPE_FIFO, &b.fd_, &b.fd_)) {
 			return error(err);
 		} else {
-			return std::make_pair(fd(a), fd(b));
+			return std::make_pair(unique_fd(a), unique_fd(b));
 		}
 	}
 
@@ -60,18 +63,45 @@ public:
 		return error(cloudabi_sys_fd_datasync(fd_));
 	}
 
-	error_or<fd> dup() {
-		cloudabi_fd_t dup_fd;
-		if (auto err = cloudabi_sys_fd_dup(fd_, &dup_fd)) {
+	error_or<unique_fd> dup() {
+		fd dup_fd;
+		if (auto err = cloudabi_sys_fd_dup(fd_, &dup_fd.fd_)) {
 			return error(err);
 		} else {
-			return fd(dup_fd);
+			return unique_fd(dup_fd);
 		}
+	}
+
+	error replace(fd const & from) {
+		return error(cloudabi_sys_fd_replace(from.fd_, fd_));
+	}
+
+	error sync() {
+		return error(cloudabi_sys_fd_sync(fd_));
+	}
+
+	// cloudabi_sys_file_ syscalls.
+
+	error file_allocate(filesize_t offset, filesize_t len) {
+		return error(cloudabi_sys_file_allocate(fd_, offset, len));
+	}
+
+	error file_create(string_view path, cloudabi_filetype_t type) {
+		return error(cloudabi_sys_file_create(fd_, path.data(), path.size(), type));
+	}
+
+	error file_symlink(string_view path, string_view contents) {
+		return error(cloudabi_sys_file_symlink(contents.data(), contents.size(), fd_, path.data(), path.size()));
+	}
+
+	error file_unlink(string_view path, cloudabi_ulflags_t flags = 0) {
+		return error(cloudabi_sys_file_unlink(fd_, path.data(), path.size(), flags));
 	}
 
 	// TODO: More syscalls.
 
-
 };
+
+inline void fd_closer::operator () (fd f) { f.close(); }
 
 }
