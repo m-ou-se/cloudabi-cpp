f3eb588d5cc50773669b91f70921385910781a7c
Maurice Bos <m-ou.se@m-ou.se>
2017-02-18 01:13:48 +0100

    Update argdata.hpp draft.

--- include/cloudabi/argdata.hpp
+++ include/cloudabi/argdata.hpp
@@ -33,34 +33,34 @@ public:
 		argdata_free(static_cast<argdata *>(p));
 	}
 
-	static std::unique_ptr<argdata> *create_binary(mstd::range<unsigned char> r) {
+	static std::unique_ptr<argdata> create_binary(mstd::range<unsigned char> r) {
 		return std::unique_ptr<argdata>(argdata_create_binary(r.data(), r.size()));
 	}
-	static std::unique_ptr<argdata> *create_buffer(mstd::range<unsigned char> r) {
+	static std::unique_ptr<argdata> create_buffer(mstd::range<unsigned char> r) {
 		return std::unique_ptr<argdata>(argdata_create_buffer(r.data(), r.size()));
 	}
-	static std::unique_ptr<argdata> *create_fd(int v) {
+	static std::unique_ptr<argdata> create_fd(int v) {
 		return std::unique_ptr<argdata>(argdata_create_fd(v));
 	}
-	static std::unique_ptr<argdata> *create_float(double v) {
+	static std::unique_ptr<argdata> create_float(double v) {
 		return std::unique_ptr<argdata>(argdata_create_float(v));
 	}
-	static std::unique_ptr<argdata> *create_int(std::uintmax_t v) {
+	static std::unique_ptr<argdata> create_int(std::uintmax_t v) {
 		return std::unique_ptr<argdata>(argdata_create_int(v));
 	}
-	static std::unique_ptr<argdata> *create_int(std::intmax_t v) {
+	static std::unique_ptr<argdata> create_int(std::intmax_t v) {
 		return std::unique_ptr<argdata>(argdata_create_int(v));
 	}
-	static std::unique_ptr<argdata> *create_int(int v) {
+	static std::unique_ptr<argdata> create_int(int v) {
 		return std::unique_ptr<argdata>(argdata_create_int(v));
 	}
-	static std::unique_ptr<argdata> *create_map(range<argdata const * const> keys, range<argdata const * const> values) {
+	static std::unique_ptr<argdata> create_map(range<argdata const * const> keys, range<argdata const * const> values) {
 		return std::unique_ptr<argdata>(argdata_create_map(keys.data(), values.data(), keys.size() < values.size() ? keys.size() : values.size());
 	}
-	static std::unique_ptr<argdata> *create_seq(range<argdata const * const> values) {
+	static std::unique_ptr<argdata> create_seq(range<argdata const * const> values) {
 		return std::unique_ptr<argdata>(argdata_create_seq(values.data(), values.size());
 	}
-	static std::unique_ptr<argdata> *create_str(string_view v) {
+	static std::unique_ptr<argdata> create_str(string_view v) {
 		return std::unique_ptr<argdata>(argdata_create_str(v.data(), v.size());
 	}
 	//TODO: static argdata create_timestamp(timespec const *);
@@ -72,43 +72,47 @@ public:
 	static constexpr argdata *bool_(bool v) { return v ? true_() : false_(); }
 
 	optional<range<unsigned char>> get_binary() const {
-		if (argdata_get_binary(this, &data, &size) == 0) return range<unsigned char>{data, size};
-		return {};
+		void const *data;
+		size_t size;
+		if (argdata_get_binary(this, &data, &size)) return {};
+		return range<unsigned char>{static_cast<unsigned char *>(data), size};
 	}
 	optional<range<unsigned char>> get_buffer() const {
-		if (argdata_get_buffer(this, &data, &size) == 0) return range<unsigned char>{data, size};
-		return {};
+		void const *data;
+		size_t size;
+		if (argdata_get_buffer(this, &data, &size)) return {};
+		return range<unsigned char>{static_cast<unsigned char *>(data), size};
 	}
 	optional<bool> get_bool() const {
 		bool r;
-		if (argdata_get_bool(this, &r) == 0) return r;
-		return {};
+		if (argdata_get_bool(this, &r)) return {};
+		return r;
 	}
 	optional<int> get_fd() const {
 		int r;
-		if (argdata_get_fd(this, &r) == 0) return r;
-		return {};
+		if (argdata_get_fd(this, &r)) return {};
+		return r;
 	}
 	optional<double> get_float() const {
 		double r;
-		if (argdata_get_float(this, &r) == 0) return r;
-		return {};
+		if (argdata_get_float(this, &r)) return {};
+		return r;
 	}
 	optional<std::intmax_t> get_int() const {
 		std::intmax_t r;
-		if (argdata_get_int(this, &r) == 0) return r;
-		return {};
+		if (argdata_get_int(this, &r)) return {};
+		return r;
 	}
 	optional<std::uintmax_t> get_uint() const {
 		std::uintmax_t r;
-		if (argdata_get_int(this, &r) == 0) return r;
-		return {};
+		if (argdata_get_int(this, &r)) return {};
+		return r;
 	}
 	optional<string_view> get_str() const {
 		char const * data;
 		size_t size;
-		if (argdata_get_str(this, &data, &size) == 0) return string_view(data, size);
-		return {};
+		if (argdata_get_str(this, &data, &size)) return {};
+		return string_view(data, size);
 	}
 	//TODO: optional<timespec> get_timestamp() const;
 
@@ -148,13 +152,13 @@ public:
 
 	optional<argdata_map> get_map() const {
 		map r;
-		if (argdata_map_iterate(this, &r.it_) == 0) return r;
-		return {};
+		if (argdata_map_iterate(this, &r.it_)) return {};
+		return r;
 	}
 	optional<argdata_seq> get_seq() const {
 		seq r;
-		if (argdata_seq_iterate(this, &r.it_) == 0) return r;
-		return {};
+		if (argdata_seq_iterate(this, &r.it_)) return {};
+		return r;
 	}
 
 	optional<argdata_map> as_map() const {
