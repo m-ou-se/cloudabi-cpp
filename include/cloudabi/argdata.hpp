c31ff2ccdc93cfcb366895430365b27d442b2376
Maurice Bos <m-ou.se@m-ou.se>
2017-02-18 12:04:06 +0100

    Update argdata draft.

--- include/cloudabi/argdata.hpp
+++ include/cloudabi/argdata.hpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <cstdio>
 #include <memory>
+#include <vector>
 
 #include <argdata.h>
 
@@ -27,17 +28,17 @@ class argdata {
 public:
 	argdata() = delete;
 	argdata(argdata const &) = delete;
-	argdata & operator=(argdata const &) = delete;
+	argdata &operator=(argdata const &) = delete;
 
-	void operator delete (void * p) {
+	void operator delete (void *p) {
 		argdata_free(static_cast<argdata *>(p));
 	}
 
+	static std::unique_ptr<argdata> create_encoded(mstd::range<unsigned char> r) {
+		return std::unique_ptr<argdata>(argdata_create_buffer(r.data(), r.size()));
+	}
 	static std::unique_ptr<argdata> create_binary(mstd::range<unsigned char> r) {
 		return std::unique_ptr<argdata>(argdata_create_binary(r.data(), r.size()));
-	}
-	static std::unique_ptr<argdata> create_buffer(mstd::range<unsigned char> r) {
-		return std::unique_ptr<argdata>(argdata_create_buffer(r.data(), r.size()));
 	}
 	static std::unique_ptr<argdata> create_fd(int v) {
 		return std::unique_ptr<argdata>(argdata_create_fd(v));
@@ -54,10 +55,10 @@ public:
 	static std::unique_ptr<argdata> create_int(int v) {
 		return std::unique_ptr<argdata>(argdata_create_int(v));
 	}
-	static std::unique_ptr<argdata> create_map(range<argdata const * const> keys, range<argdata const * const> values) {
+	static std::unique_ptr<argdata> create_map(range<argdata const *const> keys, range<argdata const *const> values) {
 		return std::unique_ptr<argdata>(argdata_create_map(keys.data(), values.data(), keys.size() < values.size() ? keys.size() : values.size());
 	}
-	static std::unique_ptr<argdata> create_seq(range<argdata const * const> values) {
+	static std::unique_ptr<argdata> create_seq(range<argdata const *const> values) {
 		return std::unique_ptr<argdata>(argdata_create_seq(values.data(), values.size());
 	}
 	static std::unique_ptr<argdata> create_str(string_view v) {
@@ -75,12 +76,6 @@ public:
 		void const *data;
 		size_t size;
 		if (argdata_get_binary(this, &data, &size)) return {};
-		return range<unsigned char>{static_cast<unsigned char *>(data), size};
-	}
-	optional<range<unsigned char>> get_buffer() const {
-		void const *data;
-		size_t size;
-		if (argdata_get_buffer(this, &data, &size)) return {};
 		return range<unsigned char>{static_cast<unsigned char *>(data), size};
 	}
 	optional<bool> get_bool() const {
@@ -109,7 +104,7 @@ public:
 		return r;
 	}
 	optional<string_view> get_str() const {
-		char const * data;
+		char const *data;
 		size_t size;
 		if (argdata_get_str(this, &data, &size)) return {};
 		return string_view(data, size);
@@ -118,7 +113,6 @@ public:
 
 	// Same as above, but return a default value (empty/zero/etc.) instead of nullopt.
 	range<unsigned char> as_binary() const { return get_binary().value_or(range<unsigned char>{}); }
-	range<unsigned char> as_buffer() const { return get_buffer().value_or(range<unsigned char>{}); }
 	bool                 as_bool  () const { return get_bool  ().value_or(                 false); }
 	int                  as_fd    () const { return get_fd    ().value_or(                    -1); }
 	double               as_float () const { return get_float ().value_or(                   0.0); }
@@ -171,8 +165,34 @@ public:
 		argdata_seq_iterate(this, &r.it_);
 		return r;
 	}
-	
-	void print_yaml(FILE * f) {
+
+	size_t encoded_size(size_t *n_fds = nullptr) const {
+		size_t r;
+		argdata_get_buffer_length(this, &r, n_fds);
+		return r;
+	}
+
+	void encode(std::vector<unsigned char> &buffer) const {
+		buffer.resize(encoded_size());
+		argdata_get_buffer(this, buffer.data(), nullptr);
+	}
+
+	void encode(std::vector<unsigned char> &buffer, std::vector<int> &fds) const {
+		size_t n_fds;
+		buffer.resize(encoded_size(&n_fds));
+		fds.resize(n_fds);
+		argdata_get_buffer(this, buffer.data(), fds.data());
+		// TODO: Check argdata_get_buffer return value? Should it always equal n_fds?
+	}
+
+	std::vector<unsigned char> encode(std::vector<int> *fds = nullptr) const {
+		std::vector<unsigned char> buffer;
+		if (fds) encode(buffer, *fds);
+		else encode(buffer);
+		return buffer;
+	}
+
+	void print_yaml(FILE *f) {
 		argdata_print_yaml(this, f);
 	}
 
